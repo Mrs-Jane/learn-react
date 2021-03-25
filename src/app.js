c4117151ac9530b15251971276a830ef6bbe4fd4
Jane Olesova <yolesova@rbc.ru>
2021-03-25 07:19:52 +0300

    lesson1 part2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        return (
-            <h1>Welcome to REACT!</h1>
-        )
-    }
+const container = document.getElementById('root');
+const title = <div>
+                <h1>Дратути!</h1>
+            </div>;
+
+let array = [
+    { name: 'msg1', text: 'Ок' },
+    { name: 'msg2', text: 'Не ок' },
+    { name: 'msg3', text: 'Че-то совсем не ок' }
+];
+
+const Content = props => {
+    const arrayObj = array.map((el, i) => <div key={ i }> { el.name }: { el.text } </div>);
+
+    return <div>
+                { title }
+                { arrayObj }
+                <button onClick={add} style={{ marginTop: '15px' }}>Это нормально</button>
+            </div>
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDom.render(<Content />, container);
+
+function add() {
+    array.push({ name: 'msg4', text: 'Нормально делай, нормально будет' });
+    ReactDom.render(<Content />, container);
+}
