e7504d20ea2c577806c9e6d8f09798f10b3275e3
Jane Olesova <yolesova@rbc.ru>
2021-03-29 19:44:56 +0300

    lesson 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,73 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
 module.exports = {
-    entry: './src/app.js',
+    context: path.resolve(__dirname, 'src'),
+    entry: path.join(__dirname, 'src', 'index.js'),
     output: {
+        path: path.join(__dirname, 'build'),
         filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
     },
+    mode: process.env.NODE_ENV,
+    target: 'browserslist',
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src', 'components'),
+            //'@styles': path.resolve(__dirname, 'src', 'styles'),
+            '@pages': path.resolve(__dirname, 'src', 'pages'),
+            '@utils': path.resolve(__dirname, 'src', 'utils'),
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        }),
+        /*new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),*/
+    ],
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
+        rules: [
+            /*{
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },*/
+            {
+                test: /\.js$|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                },
+                            ],
+                            ['@babel/preset-react', {
+                                development: isDev,
+                                runtime: 'automatic',
+                            }],
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ],
+                    },
+                },
+            },
+        ],
     }
 };
